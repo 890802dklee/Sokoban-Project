20cb2c8fa5b5d4d073d0d10def6ebaaf4ad60ab5
dongki <8343dklee@gmail.com>
2022-12-27 19:20:12 +0900

    프로그램 초기 셋팅

--- leedongki/Sokoban/Sokoban/Program.cs
+++ leedongki/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//초기 셋팅 
+Console.ResetColor();       //컬러를 초기화 한다
+Console.CursorVisible = false;            //커서를 숨긴다
+Console.Title = "동기동기";         //타이틀을 설정한다
+Console.BackgroundColor = ConsoleColor.Red;      //배경색을 설정 한다
+Console.ForegroundColor = ConsoleColor.Cyan;         //글꼴색을 설정한다
+Console.Clear();                       //출력된 모든 내용을 지운다
+
+/*
+Console.Clear();
+
+int x = 15 ;     //가로
+int y = 23;      //세로
+*/
+Console.SetCursorPosition(x, y);
+Console.Write("동기는 열심히 해야한다");
+Consolekeyinfo Kyeinfo= Console.ReadKey();
+Console.ReadLine();//Block 
+
+
+Console.WriteLine("Hello World");    
