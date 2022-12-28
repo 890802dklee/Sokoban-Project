1bbfb5dba15953100c1104814bea2b692bb38014
dongki <8343dklee@gmail.com>
2022-12-28 16:11:39 +0900

    프로젝트,

--- leedongki/Sokoban/Sokoban/Program.cs
+++ leedongki/Sokoban/Sokoban/Program.cs
@@ -1,68 +1,66 @@
-﻿//초기 셋팅 
-using System;
-
-    Console.ResetColor();       //컬러를 초기화 한다
-    Console.CursorVisible = false;            //커서를 숨긴다
-    Console.Title = "동기동기";         //타이틀을 설정한다
-    Console.BackgroundColor = ConsoleColor.Red;      //배경색을 설정 한다
-    Console.ForegroundColor = ConsoleColor.Cyan;         //글꼴색을 설정한다
-    Console.Clear();                       //출력된 모든 내용을 지운다
-                                           //게임 루프== 프레임 ( Frame)
-    int playerX = 0;
-    int playerY = 0;
-
-int boxX = 5;
-int boxY = 5;
-
+﻿//초기 세팅
+Console.ResetColor();                            // 컬러를 초기화 한다
+Console.CursorVisible = false;                   // 커서를 숨긴다
+Console.Title = "동기야 공부하자";               // 타이틀을 설정한다.
+Console.BackgroundColor = ConsoleColor.DarkGray; //배경색을 설정한다.
+Console.ForegroundColor = ConsoleColor.DarkBlue; //글꼴색을 설정한다.
+Console.Clear();    
+//출력된 모든 내용을 지운다.
+int PlayerX = 0;
+int PlayerY = 0;
+int Box1X = 3;
+int Box1Y = 3;
+int VerticalMax = 15;
+int HorizonMax = 20;
+//15 X 10
+// 게임 루프 == 프레임(Frame)
 while (true)
 {
     Console.Clear();
-    //  ------------------------------------Render----------------------
-    //플레이어 출력 하기 
-    Console.SetCursorPosition(playerX, playerY);
-    Console.Write("p");
-
-   
-    Console.SetCursorPosition(boxX, boxY);
-    Console.Write("o");
-
-    //------------------------------------ProcessInput--------------
-
-
+    // --------------------------------------------- Render -------------------------------------------------------
+    // 플레이어 출력하기
+    Console.SetCursorPosition(Box1X, Box1Y);
+    Console.Write("s");
+    Console.SetCursorPosition(PlayerX, PlayerY);
+    Console.Write("z");
+    // --------------------------------------------- ProcessInput -------------------------------------------------
     ConsoleKey key = Console.ReadKey().Key;
-
-
-    //-------------------------------UPdate------------------
-
-    //오른쪽 화살표 키를 눌렀을때 
+    // --------------------------------------------- Update -------------------------------------------------------
+    // 오른쪽 화살표키를
     if (key == ConsoleKey.RightArrow)
-    // 오른쪽으로 이동
     {
-        playerX = Math.Min(playerX + 1, 15);
+        PlayerX = Math.Min(PlayerX + 1, HorizonMax);
     }
-
     if (key == ConsoleKey.LeftArrow)
-    // 왼쪽으로 이동 
     {
-        playerX = Math.Max(playerX - 1, 0);
+        PlayerX = Math.Max(PlayerX - 1, 0);
     }
-
-    if (key == ConsoleKey.UpArrow)
-    // 위쪽으로 이동
-    {
-        playerY = Math.Max(0, playerY - 1);
-    }
-
     if (key == ConsoleKey.DownArrow)
-    // 아래쪽으로 이동
     {
-        playerY = Math.Min(playerY + 1, 10);
+        PlayerY = Math.Min(PlayerY + 1, VerticalMax);
     }
-
-    if (playerX == boxX && playerY == boxY)     
+    if (key == ConsoleKey.UpArrow)
     {
-        boxX  ++ ;
+        PlayerY = Math.Max(PlayerY - 1, 0);
     }
-     else if (playerY == boxY && playerX == boxX)
-        boxY ++ ;
+    if (PlayerX == Box1X && PlayerY == Box1Y && key == ConsoleKey.RightArrow)
+    {
+        PlayerX = Math.Min(PlayerX, HorizonMax - 1);
+        Box1X = Math.Min(Box1X + 1, HorizonMax);
+    }
+    if (PlayerX == Box1X && PlayerY == Box1Y && key == ConsoleKey.LeftArrow)
+    {
+        PlayerX = Math.Max(PlayerX, 1);
+        Box1X = Math.Max(Box1X - 1, 0);
+    }
+    if (PlayerX == Box1X && PlayerY == Box1Y && key == ConsoleKey.UpArrow)
+    {
+        PlayerY = Math.Max(PlayerY, 1);
+        Box1Y = Math.Max(Box1Y - 1, 0);
+    }
+    if (PlayerX == Box1X && PlayerY == Box1Y && key == ConsoleKey.DownArrow)
+    {
+        PlayerY = Math.Min(PlayerY, VerticalMax - 1);
+        Box1Y = Math.Min(Box1Y + 1, VerticalMax);
+    }
 }
